472ae5c135b259522a21213e9a389e79bdebe996
Alex Ooi <boonyaik@gmail.com>
2025-09-10 16:48:18 +0800

    Add files via upload

---
+++ AcquireData.py
@@ -0,0 +1,24 @@
+import requests
+
+try:
+    # Request ISS position
+    url = "http://api.open-notify.org/iss-now.json"
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract fields
+    timestamp = data['timestamp']
+    longitude = data['iss_position']['longitude']
+    latitude = data['iss_position']['latitude']
+
+    url = f"https://script.google.com/macros/s/AKfycbxinoLeZ6g/exec?Longitude={longitude}&Latitude={latitude}"
+
+    response = requests.get(url, timeout=10)
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+    print(f"Saved: {timestamp}, {longitude}, {latitude}")
+
+    response.close()
+except Exception as e:
+    print(f"Error: {e}")
+
